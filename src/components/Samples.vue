0a1fbd77333456e3323e75dc0f5bbd8606e8294e
Yuta <yuta.ok@gmail.com>
2020-04-30 00:07:07 -0700

    Added sample css file.

---
+++ src/components/Samples.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>Samples!</h1>
+    <p>This is sample page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Samples"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
