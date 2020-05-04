8071c38ded5bbbac3c58646e9c1dda4b5669bb7e
Yuta <yuta.ok@gmail.com>
2020-05-04 15:19:30 -0700

    Check in.

--- src/components/SampleUserRestaurant.vue
+++ src/components/SampleUserRestaurant.vue
@@ -39,18 +39,159 @@
             </div>
             <div class="column is-narrow w-24"></div>
           </div>
+
           <!-- Restaurant Details -->
           <div class="m-l-24 m-r-24">
-            <div class="bg-test m-t-24">hello hello</div>
-            <div class="bg-test m-t-24">hello hello yes</div>
+            <div class="m-t-24 align-center">
+              <img src="@/assets/contents/r-taku-profile.png" class="h-64 r-64 cover" />
+            </div>
+
+            <div class="m-t-8 align-center t-h6 c-text-black-high">Taku Seattle</div>
+
+            <div class="m-t-8 align-center">
+              <div class="op-button-text m-r-8">
+                <i class="material-icons">launch</i>
+                <span>Share</span>
+              </div>
+              <div class="op-button-text">
+                <i class="material-icons">rate_review</i>
+                <span>Write Review</span>
+              </div>
+            </div>
+
+            <div class="m-t-24">
+              <div class="t-h6 c-text-black-disabled">Restaurant Details</div>
+
+              <div class="bg-surface r-8 d-low m-t-8">
+                <div class="button h-160 w-full bg-form r-8 r-b-0">
+                  <i class="material-icons s-48 c-primary">place</i>
+                </div>
+
+                <div class="level m-t-16 m-l-16 m-r-16">
+                  <div class="level-left">
+                    <div class="t-body1 c-text-black-high">
+                      706 E Pike St
+                      <br />Seattle, WA 98122
+                    </div>
+                  </div>
+                  <div class="level-right">
+                    <div class="op-button-text">
+                      <i class="material-icons">file_copy</i>
+                      <span>Copy</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="m-t-8 m-l-16 m-r-16">
+                  <div class="op-button-text">
+                    <i class="material-icons">phone</i>
+                    <span>(456) 789-0123</span>
+                  </div>
+                </div>
+
+                <div class="m-t-8 m-l-16 m-r-16">
+                  <div class="op-button-text">
+                    <i class="material-icons">language</i>
+                    <span>takuseattle.com</span>
+                  </div>
+                </div>
+
+                <div class="align-center">
+                  <div class="op-status c-status-green bg-status-green-bg m-t-16">Open Now</div>
+                  <div class="p-t-8 t-caption c-status-green">Tue 4:30 AM - 9:00 PM</div>
+                </div>
+
+                <div class="align-center">
+                  <div class="op-button-text m-t-8 m-b-16">
+                    <span>View All Hours</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
         <!-- Right Column -->
         <div class="column">
           <div class="m-l-24 m-r-24">
-            <div class="bg-test m-t-24">hello hello</div>
-            <div class="bg-test m-t-24">hello hello yes</div>
+            <div class="m-t-24">
+              <!-- Menu Category -->
+              <div class="t-h6 c-text-black-disabled m-t-24">Most popular</div>
+
+              <!-- Menu Item -->
+              <div class="touchable bg-surface r-8 d-low m-t-8">
+                <div class="cols">
+                  <div class="flex-1 p-l-16 p-r-16 p-t-16 p-b-16">
+                    <div class="t-h6 c-text-black-high">Kushikatsu Special Platter</div>
+                    <div class="t-body1 c-text-black-high m-t-8">$26.00</div>
+                    <div
+                      class="t-body2 c-text-black-medium m-t-8"
+                    >11 pieces assorted kushikatsu. Served with miso soup and salad.</div>
+                  </div>
+                  <div class="p-r-16 p-t-16 p-b-16">
+                    <img
+                      src="@/assets/contents/r-taku-item-1.png"
+                      width="96"
+                      height="96"
+                      class="is-pulled-right r-4"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <!-- Menu Item -->
+              <div class="touchable bg-surface r-8 d-low m-t-8">
+                <div class="cols">
+                  <div class="flex-1 p-l-16 p-r-16 p-t-16 p-b-16">
+                    <div class="t-h6 c-text-black-high">Spicy Eggplant</div>
+                    <div class="t-body1 c-text-black-high m-t-8">$8.00</div>
+                    <div
+                      class="t-body2 c-text-black-medium m-t-8"
+                    >11 pieces assorted kushikatsu. Served with miso soup and salad.</div>
+                  </div>
+                  <div class="p-r-16 p-t-16 p-b-16">
+                    <img
+                      src="@/assets/contents/r-taku-item-2.png"
+                      width="96"
+                      height="96"
+                      class="is-pulled-right r-4"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <!-- Menu Category -->
+              <div class="t-h6 c-text-black-disabled m-t-24">Appetizers</div>
+
+              <!-- Menu Item -->
+              <div class="touchable bg-surface r-8 d-low m-t-8">
+                <div class="cols">
+                  <div class="flex-1 p-l-16 p-r-16 p-t-16 p-b-16">
+                    <div class="t-h6 c-text-black-high">Spicy Eggplant</div>
+                    <div class="t-body1 c-text-black-high m-t-8">$8.00</div>
+                    <div
+                      class="t-body2 c-text-black-medium m-t-8"
+                    >11 pieces assorted kushikatsu. Served with miso soup and salad.</div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Menu Item -->
+              <div class="touchable bg-surface r-8 d-low m-t-8">
+                <div class="cols">
+                  <div class="flex-1 p-l-16 p-r-16 p-t-16 p-b-16">
+                    <div class="t-h6 c-text-black-high">Edamame</div>
+                    <div class="t-body1 c-text-black-high m-t-8">$8.00</div>
+                  </div>
+                </div>
+              </div>
+              <div class="align-right m-t-8">
+                <div class="op-button-pill bg-form">
+                  <i class="material-icons">add</i>
+                  <span>Add</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -60,17 +201,19 @@
     </div>
 
     <!-- Footer -->
-    <div class="bg-ownplate-gray columns is-gapless is-mobile h-128">
-      <div class="column">
-        <div
-          class="is-inlineblock t-caption c-text-white-medium m-t-16 m-l-16"
-        >Operated by Singularity Society</div>
-      </div>
-      <div class="column align-right">
-        <div class="button bg-sattle-white r-36 p-l-8 p-r-8 m-r-16 m-t-16">
-          <i class="material-icons s-18 c-text-white-full m-l-8 m-r-8">language</i>
-          <span class="t-subtitle2 c-text-white-high m-r-8">English (US)</span>
-          <i class="material-icons s-18 c-text-white-high">arrow_drop_down</i>
+    <div class="m-t-24">
+      <div class="bg-ownplate-gray columns is-gapless is-mobile h-128">
+        <div class="column">
+          <div
+            class="is-inline-block t-caption c-text-white-medium m-t-16 m-l-16"
+          >Operated by Singularity Society</div>
+        </div>
+        <div class="column align-right">
+          <div class="op-button-pill bg-sattle-white m-r-16 m-t-16">
+            <i class="material-icons c-text-white-high">language</i>
+            <span class="c-text-white-high">English</span>
+            <i class="material-icons c-text-white-high">arrow_drop_down</i>
+          </div>
         </div>
       </div>
     </div>
